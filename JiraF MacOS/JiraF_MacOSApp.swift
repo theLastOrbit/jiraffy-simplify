02fd2feaa1fba6bff060a769d887c013dda7100f
MD RUBEL <hello@mdrubel.net>
2025-09-19 01:06:09 +0600

    Initial Commit

---
+++ JiraF MacOS/JiraF_MacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  JiraF_MacOSApp.swift
+//  JiraF MacOS
+//
+//  Created by MD RUBEL on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct JiraF_MacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
